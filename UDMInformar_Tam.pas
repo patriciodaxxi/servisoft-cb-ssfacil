26783909cbbd7d83f927bd3f33f9c48c70a171c1
Russimar <russimar.s@gmail.com>
2019-12-11 14:06:16 -0300

    Adicionei o field vlr_Icms_Substituto no mitens

--- UDMInformar_Tam.pas
+++ UDMInformar_Tam.pas
@@ -336,6 +336,8 @@ type
     mItensDRAWBACK: TStringField;
     mItensMEDIDA: TStringField;
     mItensPRECO_COR: TFloatField;
+    mItensVLR_ICMS_SUBSTITUTO: TFloatField;
+    mItensPERC_ICMS_FCP_5405: TFloatField;
     procedure mItensNewRecord(DataSet: TDataSet);
     procedure mTamanhoNewRecord(DataSet: TDataSet);
   private
